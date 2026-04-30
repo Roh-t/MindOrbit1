d699656c382a2ca7db8bbbb80d0781970bc6338e
Rohit Singh <singh385536@gmail.com>
2026-04-30 23:48:03 +0530

    added home and batches

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Home from './Pages/Home';
-import BatchDetails from './Pages/BatchDetails';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path='/batch' element={<BatchDetails/>}  />
     </Routes>
   )
 }
