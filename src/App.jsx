32d1ff40721000d98682e19324235cd1cf86dcc6
Rohit Singh <singh385536@gmail.com>
2026-05-01 00:26:55 +0530

    all files are commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Home from './Pages/Home';
+import SubjectGrid from './Components/SubjectGrid';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/all-classes" element={<SubjectGrid />} />
     </Routes>
   )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import SubjectGrid from './SubjectGrid';
-import DashboardSections from './MyLearning';
+import React from 'react';
+import Sidebar from '../Components/Sidebar';
+import Header from '../Components/Header';
+import BatchDetails from './BatchDetails';
+
+
 
 const Home = () => {
   return (
-    <div>
-     <SubjectGrid/>
-     <DashboardSections/>
-    </div>
-   
-  )
-}
+    <div className="flex h-screen bg-[#F8FAFC] overflow-hidden">
+      {/* Sidebar - Fixed Left */}
+      <Sidebar />
 
-export default Home
+      {/* Main Container - Scrollable Right */}
+      <div className="flex-1 flex flex-col overflow-y-auto">
+        <Header />
+        
+        <main className="p-6">
+          {/* This is where your previous BatchDetails (Grid with Course Card) goes */}
+          <BatchDetails />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
