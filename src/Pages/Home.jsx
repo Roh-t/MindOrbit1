103972d300760fe4c6607ae5700b1f1ce55da0d8
Aditya Shrivas <as958622@gmail.com>
2026-04-30 23:35:42 +0530

    subjects and study section added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import SubjectGrid from './SubjectGrid';
+import DashboardSections from './MyLearning';
 
 const Home = () => {
   return (
     <div>
-     <h1>This is home page</h1>
+     <SubjectGrid/>
+     <DashboardSections/>
     </div>
+   
   )
 }
 
